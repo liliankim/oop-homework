79ba2d2ab5fb3f0ac55c0698f390b94269edba3a
liliankim <yejean831@gmail.com>
2022-09-21 23:50:44 -0700

    making sure all changes are committed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,12 +1,14 @@
+const { default: test } = require('node:test')
 const Employee = require('../lib/Employee')
 describe('Employee constructor', () => {
     describe('test all employee properties', () => {
         test('name', () => {
             const employee = new Employee('Lilian', 12345, 'hello@email.com')
-            expect(employee.name).toEqual('Lilianlili')
+            expect(employee.name).toEqual('Lilian')
         })
     })
     describe('Employee methods', () => {
+        test('')
 
     })
 })
